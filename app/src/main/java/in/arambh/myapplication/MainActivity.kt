e1eef2f57d4e6dd63976ed11c8a37167f261ea5e
Ashutosh Bodade <52467173+ashutoshbodade@users.noreply.github.com>
2020-05-12 16:00:45 +0530

    Initial commit

---
+++ app/src/main/java/in/arambh/myapplication/MainActivity.kt
@@ -0,0 +1,37 @@
+package `in`.arambh.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var num : Int = 0
+
+       /* btnCount.setOnClickListener {
+            num = num + 2
+            txtCount.text = num.toString()
+        }*/
+
+        val btnct = btnCount
+
+        var a = txtCount.text       // get text
+
+        txtCount.text = "some text" // set text
+
+        btnct.setOnClickListener {
+           // num = num + 2
+            //txtCount.text = num.toString()
+
+            txtCount.text = "some text"
+        }
+
+
+
+
+    }
+}
